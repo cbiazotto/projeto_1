74549116fb2ffa7b3b4b9c468565955790dda0e2
Cristian <cristian.oliveira@fatec.sp.gov.br>
2023-09-16 19:01:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode () {
+    const html = document.documentElement
+    html.classList.toggle('light')
+    // if(html.classList.contains('light')){
+    //     html.classList.remove('light')
+    // } else {
+    //    html.classList.add('light')
+    // }
+
+    // pegar a tag img
+    const img = document.querySelector("#profile img")
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/avatar-light.png')
+    } else {
+        img.setAttribute('src', './assets/avatar.png')
+    }
+}
